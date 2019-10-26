0cea7d38a997ed331a31418db3776f044f0714af
David Sims <dsims37@gatech.edu>
2019-10-26 16:41:17 -0700

    Finished CNN

--- cnn.py
+++ cnn.py
@@ -13,3 +13,16 @@ classifier = Sequential()
 # 32 feature detectors that are 3x3
 # 64x64 size shape and 3 channels
 classifier.add(Conv2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Max Pooling
+# Stride of 2 - two pxs over at a time
+# Takes the maximum value in the 2x2 square
+# Size of original feature map is divided by 2 when applying max pooling
+# Reduces time complexity and less compute intensive
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Part 3 - Flattening
+# Takes all the pooled layers and flattens them into a single vector
+# Becomes the input layer for an ANN
+# Each number in the vector represents a feature (i.e. dog nose)
+classifier.add(Flatten())
